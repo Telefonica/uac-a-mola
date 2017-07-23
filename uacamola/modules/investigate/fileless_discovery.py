c7160533d55facd815b7e6a9c90874aba4087ab5
shramos <shramos@protonmail.com>
2017-07-23 15:00:15 +0200

    filess_discovery finished

--- uacamola/modules/investigate/fileless_discovery.py
+++ uacamola/modules/investigate/fileless_discovery.py
@@ -18,15 +18,19 @@ import time
 
 
 class CustomModule(Module):
+    _FORBIDDEN_PATHS = ('HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\SessionInfo\1\Desktop\NameSpace\DelegateFolders',
+                       'HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\SessionInfo\1\MyComputer\RemovableDrives',
+                       'HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\SessionInfo\1\KnownFolders',
+                       'HKCU\Control Panel\Desktop\MuiCached\MachineLanguageConfiguration')
     def __init__(self):
         information = {"Name": "Fileless discovery",
                        "Description": "This module search in a list of binaries for fileless UAC bypasses",
                        "Author": "Santiago Hernandez Ramos"}
 
         # -----------name-----default_value--description
-        options = {"binlist": [None, "Path to a list of binaries for testing", True],
-                   "xml_file": [None, "Path to a procmon like XML file", True],
-                   "sleep_time": [2, "Time between execution of the binary", False]}
+        options = {"binlist": ["binlist.txt", "Path to a list of binaries for testing", True],
+                   "xml_file": ["results.xml", "Path to a procmon like XML file", True],
+                   "sleep_time": [1, "Time between execution of the binary", False]}
 
         # Constructor of the parent class
         super(CustomModule, self).__init__(information, options)
@@ -38,65 +42,83 @@ class CustomModule(Module):
         self.events = None
         self.events_nf = None
         self.reg = None
+	self._visited = []
 
     # This module must be always implemented, it is called by the run option
     def run_module(self):
         self.init_import_modules()
-
-        self.print_info("[*] Searching for openkey not found values\n")
-        for b in self.binaries():
-            self.results[b] = []
-            self.print_info("[*] Processing %s\n" % b)
-            events = copy.deepcopy(self.p)
-            events = Filter.by_process(events, b)
-            events = Filter.by_operation(events, "RegOpenKey")
-            for e in events['RegOpenKey']:
-                path = e.find('Path').text
-                print "[*] Inserting into %s" % path
-                k = self.reg.create_key(HKCU, "\\".join(path.split("\\")[1:]))
-                self.reg.set_value(HKCU, "\\".join(
-                    path.split("\\")[1:]), "C:\\Windows\\System32\\cmd.exe")
-                previous_pid = psutil.pids()
-                self.execute(b)
-                time.sleep(int(self.args["sleep_time"]))
-                new_pid = psutil.pids()
-                if self.is_cmd_open(previous_pid):
-                    if str(path) not in self._results[b]:
-                        self._results[b].append(str(path))
-                    self.kill('cmd', previous_pid, new_pid)
-                else:
-                    self.kill(b.split('.')[0], previous_pid, new_pid)
-                self.reg.restore(k)
+	try:
+            self.print_info("[*] Searching for openkey not found values\n")
+            for b in self.binaries():
+	        self._visited = []
+                self._results[b] = []
+                self.print_info("[*] Processing %s\n" % b)
+                events = copy.deepcopy(self.p)
+                events = Filter.by_process(events, b)
+                events = Filter.by_operation(events, "RegOpenKey")
+                for e in events['RegOpenKey']:
+                    path = e.find('Path').text
+		    if path in self._visited or path in self._FORBIDDEN_PATHS:
+                        continue
+                    self._visited.append(path)
+                    print "[*] Inserting into %s" % path
+                    k = self.reg.create_key(HKCU, "\\".join(path.split("\\")[1:]))
+                    self.reg.set_value(HKCU, "\\".join(
+                        path.split("\\")[1:]), "C:\\Windows\\System32\\cmd.exe")
+                    previous_pid = psutil.pids()
+                    self.execute(b)
+                    time.sleep(int(self.args["sleep_time"]))
+                    new_pid = psutil.pids()
+                    if self.is_cmd_open(previous_pid):
+                        if str(path) not in self._results[b]:
+                            self._results[b].append(str(path))
+                        self.kill('cmd', previous_pid, new_pid)
+                    else:
+                        self.kill(b.split('.')[0], previous_pid, new_pid)
+                    self.reg.restore(k)
+        except:
+            self.print_ko("Exiting the module\n")
+            self.results()
+	    return
 
         #########################################################
 
-        self.print_info("[*] Searching for queryvalue not found values\n")
-        for b in self.binaries():
-            self.print_info("[*] Processing %s\n" % b)
-            events = copy.deepcopy(self.p)
-            events = Filter.by_process(events, b)
-            events = Filter.by_operation(events, "RegQueryValue")
-            for e in events['RegQueryValue']:
-                path = e.find('Path').text
-                print "[*] Inserting into %s" % str(path)
-                k = self.reg.create_key(
-                    HKCU, "\\".join(path.split("\\")[1:-1]))
-                self.reg.create_value(k, path.split(
-                    "\\")[-1], "C:\\Windows\\System32\\cmd.exe")
+	try:
+            self.print_info("[*] Searching for queryvalue not found values\n")
+            for b in self.binaries():
+                self._visited = []
+                self.print_info("[*] Processing %s\n" % b)
+                events = copy.deepcopy(self.p)
+                events = Filter.by_process(events, b)
+                events = Filter.by_operation(events, "RegQueryValue")
+                for e in events['RegQueryValue']:
+                    path = e.find('Path').text
+		    if path in self._visited or path in self._FORBIDDEN_PATHS:
+                        continue
+                    self._visited.append(path)
+                    print "[*] Inserting into %s" % str(path)
+                    k = self.reg.create_key(
+                        HKCU, "\\".join(path.split("\\")[1:-1]))
+                    self.reg.create_value(k, path.split(
+                        "\\")[-1], "C:\\Windows\\System32\\cmd.exe")
 
-                previous_pid = psutil.pids()
-                self.execute(b)
-                time.sleep(int(self.args["sleep_time"]))
-                new_pid = psutil.pids()
+                    previous_pid = psutil.pids()
+                    self.execute(b)
+                    time.sleep(int(self.args["sleep_time"]))
+                    new_pid = psutil.pids()
 
-                if self.is_cmd_open(previous_pid):
-                    if str(path) not in self.results[b]:
-                        self._results[b].append(str(path))
-                    self.kill('cmd', previous_pid, new_pid)
-                else:
-                    self.kill(b.split('.')[0], previous_pid, new_pid)
+                    if self.is_cmd_open(previous_pid):
+                        if str(path) not in self.results[b]:
+                            self._results[b].append(str(path))
+                        self.kill('cmd', previous_pid, new_pid)
+                    else:
+                        self.kill(b.split('.')[0], previous_pid, new_pid)
 
-                self.reg.restore(k, path.split("\\")[-1])
+                    self.reg.restore(k, path.split("\\")[-1])
+        except:
+	    self.print_ko("Exiting the module\n")
+            self.results()
+            return
 
         self.results()
 
@@ -106,15 +128,15 @@ class CustomModule(Module):
         self.reg = Registry()
 
     def results(self):
-        print "\n\n"
-        self.print_info("RESULTS")
-        print "-------\n"
+        print "\n"
+        self.print_info("RESULTS\n")
+        print "-------"
 
-        for key in self.results.keys():
-            self.print_info(key)
-            print "|" * len(key)
-            for v in self.results[key]:
+        for key in self._results.keys():
+            self.print_info(key + '\n')
+            for v in self._results[key]:
                 print "|_" + v
+        print ""
 
     def kill(self, proc, last_pids=None, new_pids=None):
         new_pids = self.last_process_created(last_pids, new_pids)
